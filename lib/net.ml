8ab254e959642275333b169fde89203d4d44d690
Olivier Hécart <olivier.hecart@adlinktech.com>
2019-02-15 16:38:23 +0100

    IO functions use Abuf

--- lib/net.ml
+++ lib/net.ml
@@ -1,89 +1,59 @@
 open Apero
-open Lwt
 
+let read sock buf len = 
+  let io_vecs = Lwt_unix.IO_vectors.create () in
+  Lwt_unix.IO_vectors.(Abuf.to_io_vecs ~idx:(Abuf.w_pos buf) ~len ~append_bytes ~append_bigarray io_vecs buf);
+  let%lwt n = Lwt_unix.readv sock io_vecs in 
+  Abuf.set_w_pos (Abuf.w_pos buf + n) buf; 
+  Lwt.return n
 
-let rec to_io_vecs bufs offset = 
-   match bufs with 
-   | [] -> []
-   | hd :: tl -> 
-     if IOBuf.available hd >= offset
-     then 
-       Lwt_bytes.{ iov_buffer = IOBuf.to_bytes hd; iov_offset = IOBuf.position hd + offset; iov_length = IOBuf.available hd - offset; }
-       :: to_io_vecs tl 0
-     else 
-       to_io_vecs tl (offset - IOBuf.available hd)
-
-
-let read_all sock buf = 
-    let rec r_read_all_r sock buf offset len read =
-      (* let%lwt _ = Logs_lwt.debug (fun m -> m "r_read_all off: %d len: %d" offset len) in *)
-      let%lwt n = Lwt_bytes.read sock buf offset len in   
-      if n <> 0 && n < len then r_read_all_r sock buf (offset + n) (len - n) (read + n)
-      else Lwt.return (read + n)
-    in
-      let pos = IOBuf.position buf in 
-      let len = (IOBuf.limit buf) - pos in 
-      r_read_all_r sock (IOBuf.to_bytes buf) pos len 0
-
-  
-
-(* let read sock buf = Lwt_bytes.read sock (IOBuf.to_bytes buf) (IOBuf.position buf) (IOBuf.limit buf) *)
-let read = read_all
-
-let write_all sock buf = 
-    let rec r_write_all_r tlen sock buf offset len  =
-      (* let%lwt _ = Logs_lwt.debug (fun m -> m "r_write_all off: %d len: %d" offset len) in *)
-      let%lwt n = Lwt_bytes.write sock buf offset len in   
-      if n < len then r_write_all_r tlen sock buf (offset + n) (len - n) 
-      else Lwt.return tlen 
-    in
-      let pos = IOBuf.position buf in 
-      let len = (IOBuf.limit buf) - pos in 
-      r_write_all_r len sock (IOBuf.to_bytes buf) pos len
-
-let write sock buf = Lwt_bytes.write sock (IOBuf.to_bytes buf) (IOBuf.position buf) (IOBuf.limit buf)
-
-let recv ?(flags=[]) sock buf =
-  match%lwt Lwt_bytes.recv sock (IOBuf.to_bytes buf) (IOBuf.position buf) (IOBuf.limit buf) flags with
-  | 0 -> fail @@ Exception (`ClosedSession `NoMsg)
-  | n -> return n
-
-
-let send ?(flags=[]) sock buf = 
-  Lwt_bytes.send sock (IOBuf.to_bytes buf) (IOBuf.position buf) (IOBuf.limit buf) flags
-
-let recvfrom ?(flags=[]) sock buf =
-  match%lwt Lwt_bytes.recvfrom sock (IOBuf.to_bytes buf) (IOBuf.position buf) (IOBuf.limit buf) flags with
-  | (0, _) -> fail @@ Exception (`ClosedSession `NoMsg)
-  | _ as r -> return r
-
-let sendto ?(flags=[]) sock buf addr = 
-  Lwt_bytes.sendto sock (IOBuf.to_bytes buf) (IOBuf.position buf) (IOBuf.limit buf) flags addr
-
-
-let recv_vec sock bs =
-  let iovec = to_io_vecs bs 0 in
-  match%lwt (Lwt_bytes.recv_msg ~socket:sock ~io_vectors:iovec) with
-  | (0, _) -> fail @@ Exception (`ClosedSession `NoMsg)
-  | _ as r -> return r
-
-let send_vec sock bs =
-  let iovec = to_io_vecs bs 0 in
-  (Lwt_bytes.send_msg ~socket:sock ~io_vectors:iovec ~fds:[])
-
-let send_vec_all sock bs = 
-  let rec send_vec_all_from sock bs offset = 
-    let total_bytes = (List.fold_left (fun a b -> a + (IOBuf.available b)) 0 bs) - offset in
-    let iovec = to_io_vecs bs offset in
-    let%lwt sent_bytes = Lwt_bytes.send_msg ~socket:sock ~io_vectors:iovec ~fds:[] in 
-    if sent_bytes < total_bytes 
+let rec read_all sock buf len = 
+  if len > 0
+  then 
+    let%lwt n = read sock buf len in
+    if n > 0 
     then 
-      let%lwt sent_bytes' = send_vec_all_from sock bs sent_bytes in
-      Lwt.return (sent_bytes + sent_bytes')
+      let%lwt n2 = read_all sock buf (len - n) in
+      Lwt.return (n + n2)
     else 
-      Lwt.return sent_bytes
-  in 
-  send_vec_all_from sock bs 0
+      Lwt.return n
+  else 
+    Lwt.return 0
+
+let write sock buf = 
+  let io_vecs = Lwt_unix.IO_vectors.create () in
+  Lwt_unix.IO_vectors.(Abuf.to_io_vecs ~idx:(Abuf.r_pos buf) ~len:(Abuf.readable_bytes buf) ~append_bytes ~append_bigarray io_vecs buf);
+  let%lwt n = Lwt_unix.writev sock io_vecs in 
+  Abuf.set_r_pos (Abuf.r_pos buf + n) buf; 
+  Lwt.return n
+
+let rec write_all sock buf = 
+  if Abuf.readable_bytes buf > 0
+  then 
+    let%lwt n = write sock buf in
+    let%lwt n2 = write_all sock buf in 
+    Lwt.return (n + n2)
+  else 
+    Lwt.return 0
+
+let read_vle sock =
+  let open Lwt.Infix in
+  let bs = Bytes.create 8 in
+  let rec extract_length (v:Vle.t) (bc:int) =
+    Lwt_unix.read sock bs bc 1 >>= function 
+    | 0 -> Lwt.fail @@ Exception (`ClosedSession (`Msg "Peer closed the session unexpectedly"))
+    | _ -> 
+      match Vle.of_char @@ Bytes.get bs bc with
+      | c when c <= 0x7fL -> Lwt.return (Vle.logor v (Vle.shift_left c (bc * 7)))
+      | c  ->
+        let x : Vle.t = Vle.shift_left (Vle.logand c  0x7fL)  (bc * 7) in
+        extract_length (Vle.logor v x) (bc + 1)
+  in extract_length Vle.zero 0
+
+let write_vle sock vle =
+  let buf = Abuf.create_bytes 8 in
+  encode_vle vle buf;
+  write_all sock buf
 
 let safe_close fd =
   Lwt.catch
@@ -92,35 +62,10 @@ let safe_close fd =
       Lwt_unix.close fd)
     (fun _ -> Lwt.return_unit)
 
-let read_vle sock buf = 
-  let rec extract_length (v:Vle.t) (bc:int) (buf: IOBuf.t) =       
-    Result.try_get 
-      ~on:(IOBuf.reset_with 0 1 buf)
-      ~run:(fun buf ->
-          match%lwt recv sock buf with
-          | 0 -> Lwt.fail @@ Exception (`ClosedSession (`Msg "Peer closed the session unexpectedly"))
-          | _ ->
-            Result.try_get 
-              ~on:(IOBuf.get_char buf)
-              ~run:(fun (b, buf) -> 
-                  match Vle.of_char b with
-                  | c when c <= 0x7fL -> Lwt.return (Vle.logor v (Vle.shift_left c (bc * 7)))
-                  | c  -> 
-                    let x : Vle.t = Vle.shift_left (Vle.logand c  0x7fL)  (bc * 7) in 
-                    extract_length  (Vle.logor v x) (bc + 1) buf)
-              ~fail_with:(fun e -> Lwt.fail @@ Exception e))            
-      ~fail_with:(fun e -> Lwt.fail @@ Exception e)        
-  in extract_length Vle.zero 0 buf 
-
-  let write_vle sock buf vle =
-    match encode_vle vle buf with 
-    | Ok buf -> send sock buf
-    | Error e -> Lwt.fail (Apero.Exception e)
-
 let connect socket locator = 
   let saddr = match locator with 
     | Locator.Locator.UdpLocator ul -> 
       Endpoint.IpEndpoint.to_sockaddr @@ Iplocator.UdpLocator.endpoint ul   
     | Locator.Locator.TcpLocator tl -> 
       Endpoint.IpEndpoint.to_sockaddr @@ Iplocator.TcpLocator.endpoint tl
-  in Lwt_unix.connect socket saddr >>= fun () -> Lwt.return socket
+  in Lwt.Infix.(Lwt_unix.connect socket saddr >>= fun () -> Lwt.return socket)
